a3950007214b7d57a6afc17c85c9b5cfaaf00588
agnestanko <tankoagnes2005@gmail.com>
2026-05-09 20:06:59 +0300

    Add initial Maven project structure

---
+++ src/main/java/com/example/trainticketing/Main.java
@@ -0,0 +1,7 @@
+package com.example.trainticketing;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Train Ticketing Application started successfully!");
+    }
+}
